4378ab172943073f54114938206a2afa1b885438
Haoran Peng <hrpeng@cs.washington.edu>
2025-07-31 23:18:04 +0000

    Fix IncludeCollector API mismatch

--- src/IncludeCollector.cc
+++ src/IncludeCollector.cc
@@ -8,13 +8,21 @@ namespace cpp2c
         llvm::StringRef FileName,
         bool IsAngled,
         clang::CharSourceRange FilenameRange,
-        const clang::FileEntry *File,
+        clang::OptionalFileEntryRef File,
         llvm::StringRef SearchPath,
         llvm::StringRef RelativePath,
         const clang::Module *Imported,
         clang::SrcMgr::CharacteristicKind FileType)
     {
-        IncludeEntriesLocs.emplace_back(File, HashLoc);
+        if (File.has_value())
+        {
+            // Implicit conversion from FileEntryRef to const clang::FileEntry * is used here.
+            IncludeEntriesLocs.emplace_back(*File, HashLoc);
+        }
+        else
+        {
+            IncludeEntriesLocs.emplace_back(nullptr, HashLoc);
+        }
     }
 
 } // namespace cpp2c
