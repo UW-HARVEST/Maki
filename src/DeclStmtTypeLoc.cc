b16854bf31bff118fd298f7e0eb5415f3faca7ea
Haoran Peng <hrpeng@cs.washington.edu>
2025-10-20 00:43:30 -0700

    Fix TypeLoc ownership problem

--- src/DeclStmtTypeLoc.cc
+++ src/DeclStmtTypeLoc.cc
@@ -2,15 +2,93 @@
 
 #include "assert.h"
 
+#include <utility>
+
 namespace cpp2c
 {
-    DeclStmtTypeLoc::DeclStmtTypeLoc(const clang::Decl *D) : D(D) {}
-    DeclStmtTypeLoc::DeclStmtTypeLoc(const clang::Stmt *ST) : ST(ST) {}
-    DeclStmtTypeLoc::DeclStmtTypeLoc(const clang::TypeLoc *TL) : TL(TL) {}
+    DeclStmtTypeLoc::DeclStmtTypeLoc(const clang::Decl *D) : D(D)
+    {
+        syncTypeLocPointer();
+        assertOneNonNull();
+    }
+
+    DeclStmtTypeLoc::DeclStmtTypeLoc(const clang::Stmt *ST) : ST(ST)
+    {
+        syncTypeLocPointer();
+        assertOneNonNull();
+    }
+
+    DeclStmtTypeLoc::DeclStmtTypeLoc(const clang::TypeLoc *TL)
+    {
+        if (TL)
+        {
+            StoredTypeLoc = *TL;
+        }
+        syncTypeLocPointer();
+        assertOneNonNull();
+    }
+
+    DeclStmtTypeLoc::DeclStmtTypeLoc(const DeclStmtTypeLoc &Other)
+        : D(Other.D),
+          ST(Other.ST),
+          StoredTypeLoc(Other.StoredTypeLoc)
+    {
+        syncTypeLocPointer();
+        assertOneNonNull();
+    }
+
+    DeclStmtTypeLoc::DeclStmtTypeLoc(DeclStmtTypeLoc &&Other) noexcept
+        : D(Other.D),
+          ST(Other.ST),
+          StoredTypeLoc(std::move(Other.StoredTypeLoc))
+    {
+        syncTypeLocPointer();
+        Other.D = nullptr;
+        Other.ST = nullptr;
+        Other.TL = nullptr;
+        Other.StoredTypeLoc.reset();
+    }
+
+    DeclStmtTypeLoc &DeclStmtTypeLoc::operator=(const DeclStmtTypeLoc &Other)
+    {
+        if (this == &Other)
+            return *this;
+
+        D = Other.D;
+        ST = Other.ST;
+        StoredTypeLoc = Other.StoredTypeLoc;
+        syncTypeLocPointer();
+        assertOneNonNull();
+        return *this;
+    }
+
+    DeclStmtTypeLoc &DeclStmtTypeLoc::operator=(DeclStmtTypeLoc &&Other) noexcept
+    {
+        if (this == &Other)
+            return *this;
+
+        D = Other.D;
+        ST = Other.ST;
+        StoredTypeLoc = std::move(Other.StoredTypeLoc);
+        syncTypeLocPointer();
+
+        Other.D = nullptr;
+        Other.ST = nullptr;
+        Other.TL = nullptr;
+        Other.StoredTypeLoc.reset();
+        assertOneNonNull();
+        return *this;
+    }
 
     inline void DeclStmtTypeLoc::assertOneNonNull()
     {
-        assert(!(D && ST) && !(D && TL) && !(ST && TL) && "More than one type");
+        const int Count = (D ? 1 : 0) + (ST ? 1 : 0) + (TL ? 1 : 0);
+        assert(Count <= 1 && "More than one type");
+    }
+
+    inline void DeclStmtTypeLoc::syncTypeLocPointer()
+    {
+        TL = StoredTypeLoc ? &*StoredTypeLoc : nullptr;
     }
 
     void DeclStmtTypeLoc::dump()
--- src/DeclStmtTypeLoc.hh
+++ src/DeclStmtTypeLoc.hh
@@ -5,21 +5,35 @@
 #include "clang/AST/TypeLoc.h"
 #include "clang/AST/ASTTypeTraits.h"
 
+#include <optional>
+
 namespace cpp2c
 {
     class DeclStmtTypeLoc
     {
     private:
         inline void assertOneNonNull();
+        inline void syncTypeLocPointer();
 
     public:
         const clang::Decl *D = nullptr;
         const clang::Stmt *ST = nullptr;
         const clang::TypeLoc *TL = nullptr;
 
+    private:
+        std::optional<clang::TypeLoc> StoredTypeLoc;
+
+    public:
+        DeclStmtTypeLoc() = default;
+
         DeclStmtTypeLoc(const clang::Decl *D);
         DeclStmtTypeLoc(const clang::Stmt *ST);
         DeclStmtTypeLoc(const clang::TypeLoc *TL);
+
+        DeclStmtTypeLoc(const DeclStmtTypeLoc &Other);
+        DeclStmtTypeLoc(DeclStmtTypeLoc &&Other) noexcept;
+        DeclStmtTypeLoc &operator=(const DeclStmtTypeLoc &Other);
+        DeclStmtTypeLoc &operator=(DeclStmtTypeLoc &&Other) noexcept;
 
         void dump();
 
@@ -27,7 +41,13 @@ namespace cpp2c
 
         bool operator==(const DeclStmtTypeLoc &Other) const
         {
-            return (D == Other.D && ST == Other.ST && TL == Other.TL);
+            if (D != Other.D || ST != Other.ST)
+                return false;
+            if (TL == nullptr && Other.TL == nullptr)
+                return true;
+            if ((TL == nullptr) != (Other.TL == nullptr))
+                return false;
+            return TL->getOpaqueData() == Other.TL->getOpaqueData();
         }
 
         clang::DynTypedNode getDynTypedNode() const
